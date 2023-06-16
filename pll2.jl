2ea37f40421f4ecc7b2f165e0c2bfa2b32bb2a3d
Matias <matias.roqueta.sz@gmail.com>
2023-06-16 13:10:59 -0300

    pll2 va flama

--- pll2.jl
+++ pll2.jl
@@ -7,66 +7,35 @@ using CUDA
 default(fontfamily="serif", legendfontsize=10, titlefontsize=12, palette=:seaborn_deep6)
 gr()
 
-msglen = 100
-scheme = :qam16
+msglen = 1000
 
-function ktoi_kernel(result, N, ks)
-    for idx = eachindex(ks)
-        @inbounds result[idx] = N[1] ÷ 16 * (ks[idx] % 16) + floor(ks[idx] ÷ 16) |> Int
-    end
-    return
-end
-
-function itoj_kernel(result, N, s, is)
+function interleave_idx_kernel(is, js, N, s, ks)
     for idx = eachindex(is)
-        @inbounds result[idx] = s[1] * floor(is[idx] ÷ s[1]) + (is[idx] + N[1] - floor((16 * is[idx]) ÷ N[1])) % s[1] |> Int
+        @inbounds is[idx] = N[1] ÷ 16 * (ks[idx] % 16) + floor(ks[idx] ÷ 16) |> Int
+        @inbounds js[idx] = s[1] * floor(is[idx] ÷ s[1]) + (is[idx] + N[1] - floor((16 * is[idx]) ÷ N[1])) % s[1] |> Int
     end
     return
 end
 
 function interleave_kernel(result, bits, idxs)
     for idx = eachindex(idxs)
-        @inbounds result[idx] = bits[idxs[idx]]
+        @inbounds result[idx] = bits[idxs[idx]+1]
     end
     return
 end
 
-function modulate(bits, bit_map)
-    n = length(bits) ÷ 2
-    return (bit_map[bits[1:n]] |> Float64) + im * (bit_map[bits[n + 1:end]] |> Float64)
-end
-
-function cu_ktoi(N, ks)
-    result_cuda = CUDA.CuArray{Int}(undef, N)
-    @cuda threads=1 blocks=1 ktoi_kernel(result_cuda, CuArray([N]), ks)
-
-    return result_cuda
-end
-
-function cu_itoj(N, s, is)
-    result_cuda = CUDA.CuArray{Int}(undef, N)
-    @cuda threads=1 blocks=1 itoj_kernel(result_cuda, CuArray([N]), CuArray([s]), is)
-
-    return result_cuda
-end
-
 N = 192
-
-function cu_indices(N)
-    arr = CuArray{Int}(0:N-1)
-    return arr
-end
-
 
 function cu_interleave(block)
     N = 192
     s = 2
-    ks = cu_indices(N)
-    is = cu_ktoi(N, ks)
-    js = cu_itoj(N, s, is)
-    result_cuda = CUDA.CuArray{Int}(undef, N)
-    @cuda threads=1 blocks=1 interleave_kernel(result_cuda, block, js)
-    return block
+    ks = CuArray{Int}(0:N-1)
+    is = CuArray{Int}(undef, N-1)
+    js = CuArray{Int}(undef, N-1)
+    result = CUDA.CuArray{Int}(undef, N)
+    @cuda threads=192 blocks=1 interleave_idx_kernel(is, js, N, s, ks)
+    @cuda threads=192 blocks=1 interleave_kernel(result, block, js)
+    return result
 end
 
 function cu_to_blocks(data::CuArray)
@@ -101,9 +70,9 @@ function cu_modulate(block_i)
 
     result_d = CuVector{ComplexF64}(undef, n)
 
-    block = reshape(block_i .|> Int, (n, nbits))
+    block = reshape(block_i .|> Int, (nbits, n))
 
-    @cuda threads=1 blocks=1 modulate_kernel(result_d, block)
+    @cuda threads=192 blocks=1 modulate_kernel(result_d, block)
 
     return result_d
 end
@@ -112,7 +81,11 @@ stream_d = CuArray(bitrand(msglen) .|> Int)
 
 block_d = cu_to_blocks(stream_d)[:,1]
 
+println(block_d)
+
 block_i = cu_interleave(block_d)
+
+println(block_i)
 
 block_m = cu_modulate(block_i)
 
